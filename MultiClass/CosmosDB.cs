9457581d051aff0acfe79a4bc911141cb36aa232
Marten Liebster <mliebster@lieb-tech.com>
2019-04-19 14:47:33 -0400

    initial

---
+++ MultiClass/CosmosDB.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace MultiClass
+{
+    public class CosmosSetting
+    {
+        public CosmosDB Cosmos { get; set; }
+        public class CosmosDB
+        {
+            public string EndpointUrl { get; set; }
+            public string PrimaryKey { get; set; }
+        }
+    }
+
+    public class CosmosDB
+    {
+        private DocumentClient client;
+
+        // ADD THIS PART TO YOUR CODE
+        public CosmosDB()
+        {
+            var data = File.ReadAllText("cosmosDbKey.json");
+            var config = JsonConvert.DeserializeObject<CosmosSetting>(data);
+
+            this.client = new DocumentClient(new Uri(config.Cosmos.EndpointUrl), config.Cosmos.PrimaryKey, new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp
+            });
+        }
+
+        public void OpenConnection()
+        {
+            client.OpenAsync().Wait();
+        }
+
+        public IQueryable<dynamic> GetDocumentQuery(string collection, string query, FeedOptions qryOptions = null)
+        {
+            if (qryOptions == null)
+                qryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+
+            return client.CreateDocumentQuery<dynamic>(
+                GetCollectionLink(collection),
+                query,
+                qryOptions);
+        }
+
+        public IQueryable<T> GetDocumentQuery<T>(string collection)
+        {
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+            return client.CreateDocumentQuery<T>(this.GetCollectionLink(collection), queryOptions);
+        }
+
+        public Uri GetCollectionLink(string collectionName)
+        {
+            return UriFactory.CreateDocumentCollectionUri("liebfeeds", collectionName);
+        }
+
+        public async Task UpsertDocument(dynamic doc, string collection)
+        {            
+            try
+            {                
+                var res = await client.UpsertDocumentAsync(GetCollectionLink(collection), doc);
+                var a = "";
+            }
+            catch (Exception ex)
+            {
+                System.Threading.Thread.Sleep(2000);                
+                try
+                {
+                    await client.UpsertDocumentAsync(GetCollectionLink(collection), doc);
+                }
+                catch (Exception ex2)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    try
+                    {
+                        await client.UpsertDocumentAsync(GetCollectionLink(collection), doc);
+                    }
+                    catch (Exception ex3)
+                    {
+                    }
+                }
+            }
+        }
+        public async Task ReplaceDocument(dynamic doc, string collection)
+        {
+            try
+            {
+                await client.ReplaceDocumentAsync(GetCollectionLink(collection), doc);
+                var a = "";
+            }
+            catch (Exception ex)
+            {
+                System.Threading.Thread.Sleep(2000);
+                try
+                {
+                    await client.UpsertDocumentAsync(GetCollectionLink(collection), doc);
+                }
+                catch (Exception ex2)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    try
+                    {
+                        await client.UpsertDocumentAsync(GetCollectionLink(collection), doc);
+                    }
+                    catch (Exception ex3)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MultiClass/NewsItems.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML.Data;
+
+namespace MultiClass
+{
+    public class NewsItem
+    {
+        [LoadColumn(0)]
+        public string ID { get; set; }
+        [LoadColumn(1)]
+        public string Feed { get; set; }
+        [LoadColumn(2)]
+        public string SiteSection { get; set; }
+        [LoadColumn(3)]
+        public string Title { get; set; }
+        [LoadColumn(4)]
+        public string Description { get; set; }
+    }
+
+    public class SectionPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Section;
+    }
+
+    public class CosmosNewsItem
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string partionKey { get; set; }
+        public string siteSection { get; set; }
+        public string id { get; set; }
+    }
+}
